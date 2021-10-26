7d949a7223ffb0d236defbb2836011317f7a1522
John Chinkes <24719595+chinkes5@users.noreply.github.com>
2021-10-26 13:23:19 -0600

    first pass on new feature

--- network_VPC.tf
+++ network_VPC.tf
@@ -1,66 +1,18 @@
-resource "aws_vpc" "vpc" {
-  cidr_block           = lookup(var.cidr_ranges, "main") #one is needed but you can make 4 more (soft limit)
-  instance_tenancy     = "default"                       #the other option is expensive!
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc"
+  cidr = lookup(var.cidr_ranges, "main")
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
   tags = {
-    Name = "main vpc"
+    Terraform = "true"
+    Environment = "dev"
   }
-}
-resource "aws_eip" "elasticIP" {
-  instance = aws_instance.app_server.id
-  vpc      = true
-  tags = {
-    name = "public-ip"
-  }
-}
-resource "aws_internet_gateway" "internetgateway" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    name = "dev internet gateway"
-  }
-}
-resource "aws_route_table" "publicroutetable" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    name = "public route table"
-  }
-}
-resource "aws_route_table_association" "publicsubnet1routetableassociation" {
-  route_table_id = aws_route_table.publicroutetable.id
-  subnet_id      = aws_subnet.publicsubnet1.id
-}
-resource "aws_route_table_association" "publicsubnet2routetableassociation" {
-  route_table_id = aws_route_table.publicroutetable.id
-  subnet_id      = aws_subnet.publicsubnet2.id
-}
-resource "aws_route_table_association" "igwroutetableassociation" {
-  #connect the IGW to the route table for access from internet
-  route_table_id = aws_route_table.publicroutetable.id
-  gateway_id = aws_internet_gateway.internetgateway.id
-}
-resource "aws_nat_gateway" "natgateway" {
-  allocation_id = "aws_eip.ip.id"
-  subnet_id     = "aws_subnet.publicsubnet1.id"
-  #subnet_id     = "aws_subnet.publicsubnet2.id"
-  tags = {
-    name = "dev nat gateway"
-  }
-  depends_on = [
-    aws_eip.elasticIP
-  ]
-}
-resource "aws_route_table" "privateroutetable" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    name = "private route table"
-  }
-}
-resource "aws_route_table_association" "privatesubnet1routetableassociation" {
-  route_table_id = aws_route_table.privateroutetable.id
-  subnet_id      = aws_subnet.privatesubnet1.id
-}
-resource "aws_route_table_association" "privatesubnet2routetableassociation" {
-  route_table_id = aws_route_table.privateroutetable.id
-  subnet_id      = aws_subnet.privatesubnet2.id
 }
