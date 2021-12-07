8db274e28561904298be718eff13f65ac888de82
John Chinkes <24719595+chinkes5@users.noreply.github.com>
2021-12-07 16:00:32 -0700

    Updates to make sure this branch works as is

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
   required_version = ">= 1.0"
 }
 
-backend "remote" {
-  organization = "Chinkes.com"
-  workspaces {
-    name = "AWS-Workspace-01"
-  }
-}
+# backend "remote" {
+#   organization = "Chinkes.com"
+#   workspaces {
+#     name = "AWS-Workspace-01"
+#   }
+# }
 provider "aws" {
   profile = "default"
   region  = var.region
