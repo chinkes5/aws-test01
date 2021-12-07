2abb566e9e41d5da40e3bf0ffdbc2879e8dc56f6
John Chinkes <24719595+chinkes5@users.noreply.github.com>
2021-12-07 16:07:54 -0700

    Tried updates to make sure this branch works as is, but still doesn't

---
+++ Run-Terraform.ps1
@@ -0,0 +1,40 @@
+If ((Read-Host "Ready to start [y/n]") -eq "y") {
+    terraform init
+
+    $response = Read-Host “Press I, P, A, D, or Q”
+    do {
+        switch ($response) {
+            "i" {
+                Write-Output "Initiallizing (again?)..."
+                terraform init
+            }
+            "p" {
+                Write-Output "Running plan..."
+                terraform plan -out plan.out
+            }
+            "a" {
+                Write-Output "Applying plan..."
+                terraform apply "plan.out"
+            }
+            "d" {
+                Write-Output "Planning destroy..."
+                terraform plan -out plan.out -destroy
+                if((Read-Host "ready to destroy [y/n]") -eq "y"){
+                    Write-Output "Destroying..."
+                    terraform apply "plan.out"
+                }
+            }
+            "q"{
+                Write-Output "All done!"
+                break
+            }
+            Default {
+                Write-Output "Didn't undestand that, please try again"
+                Write-Output "P = plan`nA = apply`nD = destroy (with verification)`nQ = quit"
+            }
+        }
+    } until ($response -eq "q")
+}  
+else {
+    Write-Output "Not ready, quitting script"
+}
